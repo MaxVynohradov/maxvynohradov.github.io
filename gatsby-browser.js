b2568bf3db2c429acea018dbe2d790d2f29d38bb
Maksym Vynohradov <mv@madappgang.com>
2020-10-10 02:22:31 +0300

    feat(blog): add code highlighting

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,12 @@
 import './src/themes/gittalk.css';
+import './src/themes/deckgoHighlightCode.css';
 
+import { defineCustomElements as deckDeckGoHighlightElement } from '@deckdeckgo/highlight-code/dist/loader';
 import React from 'react';
 
 import { App } from './src/components';
+
+deckDeckGoHighlightElement();
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore

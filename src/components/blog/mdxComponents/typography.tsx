2b62adcbeb4f56a9b6b1bae92d12642d8659df32
Maksym Vynohradov <mv@madappgang.com>
2020-10-14 03:13:25 +0300

    feat(mdx): update font weight for strong

--- src/components/blog/mdxComponents/typography.tsx
+++ src/components/blog/mdxComponents/typography.tsx
@@ -9,7 +9,8 @@ export const MdxInlineCodeBlock = styled.pre``;
 export const MdxEmText = styled.em``;
 
 export const MdxStrongText = styled.strong`
-  font-weight: bold;
+  font-weight: 800;
+  text-shadow: 0 1px 1px rgba(0, 0, 0, 0.4);
 `;
 
 export const MdxDeleteText = styled.del``;

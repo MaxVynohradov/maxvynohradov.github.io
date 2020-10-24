f4056a6b5e2f56ac6af8e6990fdb603b5e14b2a9
Maksym Vynohradov <mv@madappgang.com>
2020-10-24 03:59:00 +0300

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-39679d53bc6a0e48fa4b.js"
+    "url": "webpack-runtime-fb74a5f8a57d51c478eb.js"
   },
   {
     "url": "framework-376edee25eb5f5cd8260.js"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1a36290e5fb65a3a935eb5c0e74a201a"
+    "revision": "c26e8088c1a2a4d664e701928e3e7290"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-c9875e2473a7c518755f.js"
